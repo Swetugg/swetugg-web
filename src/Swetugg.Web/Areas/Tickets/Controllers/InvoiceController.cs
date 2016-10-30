ea5d055d5626b4fe8886bc6dc3eda4d4087627df
Cecilia Wirén <cecilia.wiren@live.se>
2016-10-30 23:22:17 +0100

    start on invoice request form

---
+++ src/Swetugg.Web/Areas/Tickets/Controllers/InvoiceController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using SendGrid;
+using Swetugg.Web.Models;
+
+namespace Swetugg.Web.Areas.Tickets.Controllers
+{
+    [RequireHttps]
+    [RouteArea("Tickets", AreaPrefix = "tickets")]
+    public class InvoiceController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private bool _useSendGrid;
+
+        public InvoiceController(ApplicationDbContext dbContext)
+        {
+            var str = ConfigurationManager.AppSettings["SendGrid_Messaging_Enabled"];
+
+            if (!string.IsNullOrEmpty(str) && str.Equals("true", StringComparison.InvariantCultureIgnoreCase))
+                _useSendGrid = true;
+
+            _dbContext = dbContext;
+        }
+
+        [Route("")]
+        public ActionResult Index()
+        {
+            var request = new InvoiceRequest();
+
+            return View(request);
+        }
+
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+
+
+
+        public async Task<ActionResult> InvoiceRequest(InvoiceRequest request)
+        {
+
+            var m = $"";
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(request).State = EntityState.Added;
+                await _dbContext.SaveChangesAsync();
+            }
+
+            if (_useSendGrid)
+            //https://azure.microsoft.com/sv-se/documentation/articles/sendgrid-dotnet-how-to-send-email/
+            {
+
+                var username = System.Environment.GetEnvironmentVariable("SENDGRID_USER");
+                var pswd = System.Environment.GetEnvironmentVariable("SENDGRID_PASS");
+                var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+
+                var message = new SendGridMessage();
+                //lägga beställarens mail här? om den är fel, hur blir det då?
+                message.From = new MailAddress("info@swetugg.se");
+                message.AddTo("info@swetugg.se");
+                message.Subject = "Fakturaförfrågan";
+                message.Text = $"";
+
+                //Credentials
+                var credentials = new NetworkCredential(username, pswd);
+                var transportWeb = new SendGrid.Web(credentials);
+
+                //API keys
+                //var apiKey = "your_sendgrid_api_key";
+                //var transportWeb = new SendGrid.Web(apiKey);
+
+                await transportWeb.DeliverAsync(message);
+
+            }
+            return RedirectToAction("Thanks");
+        }
+    }
+}
---
+++ src/Swetugg.Web/Models/InvoiceRequest.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Swetugg.Web.Models
+{
+    public class InvoiceRequest
+    {
+        public int Id { get; set; }
+        public int ConferenceId { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        [Display(Name = "Namn (för- och efternamn)")]
+        public string InvoiceReciver { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-post")]
+        public string Email { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        [Display(Name = "Telefon")]
+        public string Phone { get; set; }
+
+        [StringLength(250)]
+        [Required]
+        [Display(Name = "Företag")]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(13)]
+        [Display(Name = "Organisationsnummer")]
+        public string OrganisationNumber { get; set; }
+
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Fakturaadress")]
+        public string InvoiceAddress { get; set; }
+
+        [Required]
+        [StringLength(6)]
+        [Display(Name = "Postnummer")]
+        public string PostNumber { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        [Display(Name = "Ort")]
+        public string PostCity { get; set; }
+
+
+        [StringLength(250)]
+        [Display(Name = "Märk fakturan med")]
+        public string InvoiceNote { get; set; }
+
+
+        [EmailAddress]
+        [Required]
+        [Display(Name = "Epost att skicka fakturan till")]
+        public string InvoiceEmail { get; set; }
+
+        [Range(5, 20)]
+        [Required]
+        [Display(Name = "Antal biljetter")]
+        public int NumberOfTickets { get; set; }
+
+        [StringLength(250)]
+        [Display(Name = "Meddelande till Swetugg")]
+        public string Note { get; set; }
+    }
+}
